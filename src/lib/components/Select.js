c5b6c3afa2e01425accb0c53a31f8a2120fdea10
Chris5477 <petitcolaschristopher@gmail.com>
2022-02-05 15:11:31 +0100

    finish

--- src/lib/components/Select.js
+++ src/lib/components/Select.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 const Select = ({ id, textLabel, selectClass, handleChange, optionClass, arr }) => {
 	return (
 		<>
 			<label htmlFor={id}>{textLabel}</label>
-			<select className={selectClass} id={id} value={""} onChange={(e) => handleChange && handleChange(e)}>
+			<select className={selectClass} id={id} onChange={(e) => handleChange && handleChange(e)}>
 				{arr.map(({ value, text }, index) => (
 					<option className={optionClass} key={`index ${index}`} value={value}>
 						{text}
